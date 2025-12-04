d01f84705685cf8ee28c2b1ec8522ed13da80c78
Mateus Antunes <mateus.antunes95@gmail.com>
2025-12-04 17:57:16 -0300

    add BaseRepository

---
+++ backend/repositories/BaseRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace repositories;
+
+use yii\db\ActiveRecord;
+
+abstract class BaseRepository
+{
+    protected ActiveRecord $model;
+
+    public function __construct(ActiveRecord $model)
+    {
+        $this->model = $model;
+    }
+
+    public function findById(int $id): ?ActiveRecord
+    {
+        return $this->model::findOne($id);
+    }
+
+    public function findAll(): array
+    {
+        return $this->model::find()->all();
+    }
+
+    public function save(ActiveRecord $model): bool
+    {
+        return $model->save();
+    }
+
+    public function delete(ActiveRecord $model): bool
+    {
+        return (bool) $model->delete();
+    }
+
+    public function findBy(string $field, mixed $value): ?ActiveRecord
+    {
+        return $this->model::find()
+            ->where([$field => $value])
+            ->one();
+    }
+}
