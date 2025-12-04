4963ab8814de1526367930da1869e9cba3340984
Mateus Antunes <mateus.antunes95@gmail.com>
2025-12-04 17:58:11 -0300

    add Migraion club table

---
+++ backend/migrations/m251204_204330_create_club_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%club}}`.
+ */
+class m251204_204330_create_club_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%club}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255)->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%club}}');
+    }
+}
