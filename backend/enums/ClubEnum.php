94c7975bce7fcab2c9a704cda63842303b47049c
Mateus Antunes <mateus.antunes95@gmail.com>
2025-12-12 15:27:30 -0300

    add enum de club

---
+++ backend/enums/ClubEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\enums;
+
+enum ClubEnum: string
+{
+    case Flamengo       = 'Clube_de_Regatas_Flamengo';
+    case Palmeiras      = 'Sociedade_Esportiva_Palmeiras';
+    case Cruzeiro       = 'Cruzeiro_Esporte_Clube';
+    case Mirassol_SP    = 'Mirassol_Futebol_Clube';
+    case Fluminense     = 'Fluminense_Football_Club';
+    case Botafogo       = 'Botafogo_de_Futebol_e_Regatas';
+    case EC_Bahia       = 'Esporte_Clube_Bahia';
+    case Sao_Paulo      = 'São_Paulo_Futebol_Clube';
+    case Gremio         = 'Grêmio_Foot-Ball_Porto_Alegrense';
+    case RB_Bragantino  = 'Red_Bull_Bragantino';
+    case Atletico_MG    = 'Clube_Atlético_Mineiro';
+    case Santos         = 'Santos_Futebol_Clube';
+    case Corinthians    = 'Sport_Club_Corinthians_Paulista';
+    case Vasco_da_Gama  = 'Club_de_Regatas_Vasco_da_Gama';
+    case EC_Vitoria     = 'Esporte_Clube_Vitória';
+    case Internacional  = 'Sport_Club_Internacional';
+    case Ceara_SC       = 'Ceará_Sporting_Club';
+    case Fortaleza      = 'Fortaleza_Esporte_Clube';
+    case Juventude      = 'Esporte_Clube_Juventude';
+    case Sport_Recife   = 'Sport_Club_Recife';
+}
