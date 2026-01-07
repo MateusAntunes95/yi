8cf7fda64b0f0e72e2750405dbf9407ce1e49365
Mateus Antunes <mateus.antunes95@gmail.com>
2026-01-07 16:13:19 -0300

    create makeHeader

--- backend/services/BaseService.php
+++ backend/services/BaseService.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace app\services;
-
-abstract class BaseService
+ class BaseService
 {
     protected $repository;
 
     public function __construct($repository)
     {
         $this->repository = $repository;
+    }
+
+    protected function makeHeader(array $fields): array
+    {
+        $header = [];
+        foreach ($fields as $field) {
+            $header[] = [
+                'key' => $field,
+                'label' => $field
+            ];
+        }
+        return $header;
     }
 }
