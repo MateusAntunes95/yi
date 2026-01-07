78b72df01ebf23d0c2d9bd31dffc8d02cbd271a4
Mateus Antunes <mateus.antunes95@gmail.com>
2026-01-07 16:12:45 -0300

    create findDetailDtoByName

--- backend/repositories/ClubRepository.php
+++ backend/repositories/ClubRepository.php
@@ -15,4 +15,21 @@ class ClubRepository extends BaseRepository
     {
         return $this->findBy('name', $name);
     }
+
+    public function findDetailDtoByName(string $name): \app\dto\ClubDetailDto
+    {
+        $club = $this->findByName($name);
+
+        $detailArray = $club && $club->detail
+            ? json_decode($club->detail, true) ?? []
+            : [];
+
+        $dto = new \app\dto\ClubDetailDto();
+
+        foreach ($dto as $key => $_) {
+            $dto->$key = $detailArray[$key] ?? '?';
+        }
+
+        return $dto;
+    }
 }
