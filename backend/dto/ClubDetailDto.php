8a8ee48b6f5660978c1030f07fb84507e59d2b0a
Mateus Antunes <mateus.antunes95@gmail.com>
2026-01-07 16:11:16 -0300

    add to array

--- backend/dto/ClubDetailDto.php
+++ backend/dto/ClubDetailDto.php
@@ -4,11 +4,24 @@ namespace app\dto;
 
 class ClubDetailDto
 {
-    public string $name;
+    public string $name = '';
     public array $nickname = [];
     public array $mascot = [];
-    public string $founded;
-    public string $stadium;
-    public string $capacity;
-    public string $location;
+    public string $founded = '';
+    public string $stadium = '';
+    public string $capacity = '';
+    public string $location = '';
+
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'nickname' => $this->nickname,
+            'mascot' => $this->mascot,
+            'founded' => $this->founded,
+            'stadium' => $this->stadium,
+            'capacity' => $this->capacity,
+            'location' => $this->location,
+        ];
+    }
 }
